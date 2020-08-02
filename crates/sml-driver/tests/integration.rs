0ee96a35727d652f15bcd9bd820f1852f79ae728
Michael Lazear <lazear@scripps.edu>
2020-08-02 15:54:41 -0700

    Remove .exe from hardcoded path

--- crates/sml-driver/tests/integration.rs
+++ crates/sml-driver/tests/integration.rs
@@ -2,6 +2,6 @@ use goldentests::{TestConfig, TestResult};
 
 #[test]
 fn goldentests() -> TestResult<()> {
-    let config = TestConfig::new("../../target/debug/sml-driver.exe", "../../tests/", "-- ")?;
+    let config = TestConfig::new("../../target/debug/sml-driver", "../../tests/", "-- ")?;
     config.run_tests()
 }
